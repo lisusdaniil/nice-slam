fixed weighting for background sphere (now rooted in theory)

src/common.py
@@ -240,11 +240,13 @@ def raw2outputs_nerf_color(raw, z_vals, rays_d, occupancy=False, raw_bg=None, de
         # original nerf, volume density
         alpha = raw2alpha(raw[..., -1], dists)  # (N_rays, N_samples)
     # Get cumulative weights along rays
-    weights = alpha.float() * torch.cumprod(torch.cat([torch.ones((alpha.shape[0], 1)).to(
-        device).float(), (1.-alpha + 1e-10).float()], -1).float(), -1) #(N_rays, N_samples + 1)
-    # divide into foreground and background weights. Background is residual after all points
-    weights_fg = weights[:, :-1]
-    weights_bg = 1-weights[:,-1]
+    T_i = torch.cumprod(torch.cat([torch.ones((alpha.shape[0], 1)).to(
+        device).float(), (1.-alpha + 1e-10).float()], -1).float(), -1)
+    # Foreground weighting
+    weights_fg = alpha.float() * T_i[:, :-1] #(N_rays, N_samples)
+    # Background weighting - transmittence only since background NeRF modeled as single color
+    weights_bg = T_i[:,-1]
+    
     # Mult weights by color
     rgb_map_fg = torch.sum(weights_fg[..., None] * rgb, -2)  # (N_rays, 3)
     # if background is populated then get colors

src/utils/Renderer.py
@@ -190,9 +190,9 @@ class Renderer(object):
         # Note: here we are temporarily hijacking the color grid inputs to update 
         # the sphere grid voxels (actually pixels)
         if 'grid_sphere' in c.keys():
-            # Make copy of vars
+            # Make copy of features
             c_bg = c.clone()
-            # Replace color grid
+            # Replace color grid with sphere grid
             c_bg['grid_color'] = c['grid_sphere']
             # Generate points from ray directions
             polar = torch.arccos(rays_d[:,2])
@@ -206,7 +206,7 @@ class Renderer(object):
             raw_bg = None
             
         depth, uncertainty, color, weights = raw2outputs_nerf_color(
-            raw, z_vals, rays_d, occupancy=self.occupancy, device=device)
+            raw, z_vals, rays_d, raw_bg=raw_bg, occupancy=self.occupancy, device=device)
         
         if N_importance > 0:
             z_vals_mid = .5 * (z_vals[..., 1:] + z_vals[..., :-1])